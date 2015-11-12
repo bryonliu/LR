2f2f8a561db265c7705ef423d47d55fddebb92c3
bryonliu <liubshwzc@163.com>
2015-11-12 19:03:24 +0800

    add first

---
+++ app/src/main/java/com/top/bryon/lr/ui/activity/MainActivity.java
@@ -0,0 +1,28 @@
+package com.top.bryon.lr.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.top.bryon.lr.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.common_bar, menu);
+        MenuItem settingMenu = menu.findItem(R.id.action_settings);
+        MenuItem quitMenu = menu.findItem(R.id.action_quit);
+        settingMenu.setVisible(false);
+        quitMenu.setVisible(false);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
